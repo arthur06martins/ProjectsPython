eab7e4de572bb24332c8a8e5cb69a64b4e99de15
arthur06martins <arthur12martins12@gmail.com>
2024-07-01 17:57:57 -0300

    dia 3 finalizado

--- 100DaysOfCode/Day3/obs.py
+++ 100DaysOfCode/Day3/obs.py
@@ -1,6 +1,6 @@
 print("Welcome to the rollercoaster!")
 height =  int(input("What is your height in cm?\n"))
-
+bill =  0
 # maior >
 # menor <
 # maior ou igual >=
@@ -9,5 +9,25 @@ height =  int(input("What is your height in cm?\n"))
 # diferente !=
 if height > 120:
     print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+
+    if age < 12:
+        bill = 5
+        print("child tickets are $5")
+    elif age <= 18:
+        bill = 7
+        print ("youth tickets are $7")
+    else:
+        bill = 12
+        print("Adult tickets are $12")
+
+    wants_photo = input("Do you want a photo taken? y or n.")
+    if wants_photo == "y":
+        bill += 3
+        
+    print(f"Tour final bill is{bill} dollars ")
+    
+
+
 else:
     print("Sorry, you have to grow taller before you can ride")
